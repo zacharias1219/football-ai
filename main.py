5f6c6d76ce931588f2be6568d071ccc80bc9e92d
Richard Abishai <rickoshade1891@gmail.com>
2024-10-26 17:17:28 +0530

    Refactor code to add soccer pitch configuration and import necessary modules and classes

--- main.py
+++ main.py
@@ -9,4 +9,73 @@ import supervision as sv
 from tqdm import tqdm
 from ultralytics import YOLO
 
-from sports.annotations.soccer import dr
+from sports.annotators.soccer import draw_pitch, draw_paths_on_pitch
+from sports.common.ball import BallTracker, BallAnnotator
+from sports.common.team import TeamClassifier
+from sports.common.view import ViewTransformer
+from sports.configs.soccer import SoccerPitchConfiguration
+
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PLAYER_DETECTIONS_MODEL_PATH = os.path.join(PARENT_DIR, 'data/football-player-detection.pt')
+PITCH_DETECTION_MODEL_PATH = os.path.join(PARENT_DIR, 'data/football-pitch-detection.pt')
+BALL_DETECTION_MODEL_PATH = os.path.join(PARENT_DIR, 'data/football-ball-detection.pt')
+
+BALL_CLASS_ID = 0
+GOALKEEPER_CLASS_ID = 1
+PLAYER_CLASS_ID = 2
+REFEREE_CLASS_ID = 3
+
+STRIDE = 60
+CONFIG = SoccerPitchConfiguration()
+
+COLORS = ['#FF1493','#00BFFF','#FF6347','#FFD700']
+VERTEX_LABEL_ANNOTATOR = sv.VerterLabelAnnotator(
+    color=[sv.Color.from_hex(color) for color in CONFIG.colors],
+    text_color=sv.Color.from_hex('#FFFFFF'),
+    border_radius=5,
+    text_thickness=1,
+    text_scale=0.5,
+    text_padding=5,
+)
+EDGE_ANNOTATOR = sv.EdgeAnnotator(
+    color=sv.Color.from_hex('#FF1493'),
+    thickness=2,
+    edges=CONFIG.edges,
+)
+TRIANGLE_ANNOTATOR = sv.TriangleAnnotator(
+    color=sv.Color.from_hex('#FF1493'),
+    base=20,
+    height=15,
+)
+BOX_ANNOTATOR = sv.BoxAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    thickness=2
+)
+ELLIPSE_ANNOTATOR = sv.EllipseAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    thickness=2
+)
+BOX_LABEL_ANNOTATOR = sv.LabelAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    text_color=sv.Color.from_hex('#FFFFFF'),
+    text_padding=5,
+    text_thickness=1,
+)
+ELLIPSE_LABEL_ANNOTATOR = sv.LabelAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    text_color=sv.Color.from_hex('#FFFFFF'),
+    text_padding=5,
+    text_thickness=1,
+    text_position=sv.Position.BOTTOM_CENTER,
+)
+
+class Mode(enum):
+    """
+    Enum class representing different modes of operation for soccer AI video analysis.
+    """
+    PITCH_DETECTION = 'PITCH_DETECTION'
+    PLAYER_DETECTION = 'PLAYER_DETECTION'
+    BALL_DETECTION = 'BALL_DETECTION'
+    PLAYER_TRACKING = 'PLAYER_TRACKING'
+    TEAM_CLASSIFICATION = 'TEAM_CLASSIFICATION'
+    RADAR = 'RADAR'
